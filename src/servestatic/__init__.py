0a66eec860e2d3ce4949a7b1b752e72066e78a37
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2024-09-15 08:49:08 -0700

    Transitions all possible items to use hatch and modern configuration
    flake8 does not accept pyproject.toml

--- src/servestatic/__init__.py
+++ src/servestatic/__init__.py
@@ -3,4 +3,6 @@ from __future__ import annotations
 from servestatic.asgi import ServeStaticASGI
 from servestatic.wsgi import ServeStatic
 
+__version__ = "2.0.1"
+
 __all__ = ["ServeStaticASGI", "ServeStatic"]
