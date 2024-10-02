597578bb8c073e1fd67509861af10c1bebdf27ec
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-10-02 15:21:06 -0700

    Add another nocov

--- tests/django_urls.py
+++ tests/django_urls.py
@@ -1,10 +1,9 @@
-# pragma: no cover
 from __future__ import annotations
 
 from django.urls import path
 
 
-def avoid_django_default_welcome_page():
+def avoid_django_default_welcome_page():  # pragma: no cover
     pass
 
 
