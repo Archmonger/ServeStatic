a68361188209702919b16d4391ad4e0de14215c6
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-10-02 15:07:16 -0700

    fix typos

--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -3,25 +3,25 @@ from __future__ import annotations
 from servestatic.utils import ensure_leading_trailing_slash
 
 
-def test_none(self):
+def test_none():
     assert ensure_leading_trailing_slash(None) == "/"
 
 
-def test_empty(self):
+def test_empty():
     assert ensure_leading_trailing_slash("") == "/"
 
 
-def test_slash(self):
+def test_slash():
     assert ensure_leading_trailing_slash("/") == "/"
 
 
-def test_contents(self):
+def test_contents():
     assert ensure_leading_trailing_slash("/foo/") == "/foo/"
 
 
-def test_leading(self):
-    assert ensure_leading_trailing_slash("/foo") == "/foo"
+def test_leading():
+    assert ensure_leading_trailing_slash("/foo") == "/foo/"
 
 
-def test_trailing(self):
-    assert ensure_leading_trailing_slash("foo/") == "/foo"
+def test_trailing():
+    assert ensure_leading_trailing_slash("foo/") == "/foo/"
