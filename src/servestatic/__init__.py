2500560c209c83b376fa2e48077c2b6dd71de0b4
Archmonger <16909269+Archmonger@users.noreply.github.com>
2025-06-03 22:24:31 -0700

    v3.0.2

--- src/servestatic/__init__.py
+++ src/servestatic/__init__.py
@@ -3,6 +3,6 @@ from __future__ import annotations
 from servestatic.asgi import ServeStaticASGI
 from servestatic.wsgi import ServeStatic
 
-__version__ = "3.0.1"
+__version__ = "3.0.2"
 
 __all__ = ["ServeStatic", "ServeStaticASGI"]
