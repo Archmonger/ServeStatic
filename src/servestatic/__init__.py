6cb0d22488da52fed34b4f1c00e9f52491c0745c
Archmonger <archiethemonger@gmail.com>
2026-09-07 02:01:38 +0000

    4.3.4

--- src/servestatic/__init__.py
+++ src/servestatic/__init__.py
@@ -3,6 +3,6 @@ from __future__ import annotations
 from servestatic.asgi import ServeStaticASGI
 from servestatic.wsgi import ServeStatic
 
-__version__ = "4.3.3"
+__version__ = "4.3.4"
 
 __all__ = ["ServeStatic", "ServeStaticASGI"]
